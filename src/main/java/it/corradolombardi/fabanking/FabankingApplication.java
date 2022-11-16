30036257f17c479122d2c785ad046ab67caa5b9d
corrad82 <corrado.lombardi@gmail.com>
2022-11-16 23:53:21 +0100

    first commit

---
+++ src/main/java/it/corradolombardi/fabanking/FabankingApplication.java
@@ -0,0 +1,13 @@
+package it.corradolombardi.fabanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FabankingApplication.class, args);
+    }
+
+}
