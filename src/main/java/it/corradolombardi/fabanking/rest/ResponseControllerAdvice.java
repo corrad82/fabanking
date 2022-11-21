29dad7265adaf59378bbe90eb4a30e59eeffbf75
corrado lombardi <corrado.lombardi@4science.com>
2022-11-22 00:30:48 +0100

    added message in json representation in case of balance unavailable exception

--- src/main/java/it/corradolombardi/fabanking/rest/ResponseControllerAdvice.java
+++ src/main/java/it/corradolombardi/fabanking/rest/ResponseControllerAdvice.java
@@ -1,8 +1,12 @@
 package it.corradolombardi.fabanking.rest;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import it.corradolombardi.fabanking.balance.BalanceUnavailableException;
 import it.corradolombardi.fabanking.model.AccountNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,9 +23,15 @@ public class ResponseControllerAdvice extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler({BalanceUnavailableException.class})
-    protected ResponseEntity<Object> balanceNotAvailable(BalanceUnavailableException exception) {
+    protected ResponseEntity<String> balanceNotAvailable(BalanceUnavailableException exception) {
         log.error(exception.getMessage(), exception);
-        return ResponseEntity.internalServerError()
-            .body(exception.getMessage());
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("error", exception.getMessage());
+        return ResponseEntity.internalServerError().contentType(MediaType.APPLICATION_JSON)
+            .body(jsonObject.toString());
     }
+
+
+
+
 }
