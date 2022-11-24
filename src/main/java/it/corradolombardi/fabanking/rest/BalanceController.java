53f6f2748e85c9a166d77d2b2cc82d565cbd28b2
corrado lombardi <corrado.lombardi@4science.com>
2022-11-24 22:54:41 +0100

    refactoring

--- src/main/java/it/corradolombardi/fabanking/rest/BalanceController.java
+++ src/main/java/it/corradolombardi/fabanking/rest/BalanceController.java
@@ -6,14 +6,12 @@ import it.corradolombardi.fabanking.balance.InformationUnavailableException;
 import it.corradolombardi.fabanking.model.AccountNotFoundException;
 import it.corradolombardi.fabanking.model.BalanceRest;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
 @RequiredArgsConstructor
 public class BalanceController {
 
